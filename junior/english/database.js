260c3082e6a6de12e9088eb59797ae251d8b1502
JutorLiao-1991 <sonicalexsonic@gmail.com>
2025-12-27 10:55:22 +0800

    Create database.js

---
+++ junior/english/database.js
@@ -0,0 +1,16 @@
+// 這是資料庫檔案，由 Admin 系統生成
+var jutorDB = {
+    "國一": {
+        "翰林版": {
+            "Book 1 Unit 1": [
+                {
+                    "en": "student",
+                    "ch": "學生",
+                    "sentence": "I am a student."
+                }
+            ]
+        }
+    },
+    "國二": {},
+    "國三": {}
+};
