da5243483b48383a8db5195e8f43c9e3dfc5b687
JutorLiao-1991 <sonicalexsonic@gmail.com>
2025-12-27 11:31:23 +0800

    Update database via Jutor Admin

--- junior/english/database.js
+++ junior/english/database.js
@@ -1,16 +1,27 @@
-// 這是資料庫檔案，由 Admin 系統生成
 var jutorDB = {
-    "國一": {
-        "翰林版": {
-            "Book 1 Unit 1": [
-                {
-                    "en": "student",
-                    "ch": "學生",
-                    "sentence": "I am a student."
-                }
-            ]
+  "國一": {
+    "翰林版": {
+      "Book 1 Unit 1": [
+        {
+          "en": "student",
+          "ch": "學生",
+          "sentence": "I am a student."
         }
-    },
-    "國二": {},
-    "國三": {}
+      ],
+      "Book 1 Unit 5": [
+        {
+          "en": "apple",
+          "ch": "蘋果",
+          "sentence": "I have an apple."
+        },
+        {
+          "en": "banana",
+          "ch": "香蕉",
+          "sentence": "He likes bananas."
+        }
+      ]
+    }
+  },
+  "國二": {},
+  "國三": {}
 };
