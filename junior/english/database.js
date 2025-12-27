4a9a481eb33c4dff79769c7da0e7efb3641ea7f1
JutorLiao-1991 <sonicalexsonic@gmail.com>
2025-12-27 11:38:45 +0800

    Update database via Jutor Admin

--- junior/english/database.js
+++ junior/english/database.js
@@ -10,14 +10,124 @@ var jutorDB = {
       ],
       "Book 1 Unit 5": [
         {
-          "en": "apple",
-          "ch": "蘋果",
-          "sentence": "I have an apple."
+          "en": "date",
+          "ch": "日期"
         },
         {
-          "en": "banana",
-          "ch": "香蕉",
-          "sentence": "He likes bananas."
+          "en": "month",
+          "ch": "月份"
+        },
+        {
+          "en": "holiday",
+          "ch": "節日"
+        },
+        {
+          "en": "animal",
+          "ch": "動物"
+        },
+        {
+          "en": "Christmas",
+          "ch": "聖誕節"
+        },
+        {
+          "en": "January",
+          "ch": "一月"
+        },
+        {
+          "en": "February",
+          "ch": "二月"
+        },
+        {
+          "en": "March",
+          "ch": "三月"
+        },
+        {
+          "en": "April",
+          "ch": "四月"
+        },
+        {
+          "en": "May",
+          "ch": "五月"
+        },
+        {
+          "en": "June",
+          "ch": "六月"
+        },
+        {
+          "en": "July",
+          "ch": "七月"
+        },
+        {
+          "en": "August",
+          "ch": "八月"
+        },
+        {
+          "en": "September",
+          "ch": "九月"
+        },
+        {
+          "en": "October",
+          "ch": "十月"
+        },
+        {
+          "en": "November",
+          "ch": "十一月"
+        },
+        {
+          "en": "December",
+          "ch": "十二月"
+        },
+        {
+          "en": "first",
+          "ch": "第一"
+        },
+        {
+          "en": "second",
+          "ch": "第二"
+        },
+        {
+          "en": "third",
+          "ch": "第三"
+        },
+        {
+          "en": "important",
+          "ch": "重要的"
+        },
+        {
+          "en": "fifth",
+          "ch": "第五"
+        },
+        {
+          "en": "ninth",
+          "ch": "第九"
+        },
+        {
+          "en": "twelfth",
+          "ch": "第十二"
+        },
+        {
+          "en": "twentieth",
+          "ch": "第二十"
+        },
+        {
+          "en": "thirty-fourth",
+          "ch": "第三十四"
+        },
+        {
+          "en": "What's the date today?",
+          "ch": "今天是幾月幾號？"
+        },
+        {
+          "en": "It's the fifteenth of December.",
+          "ch": "今天是十二月十五號"
+        },
+        {
+          "en": "What date is the English class?",
+          "ch": "英文課是幾月幾號？"
+        },
+        {
+          "en": "It's on January twentieth.",
+          "ch": "是在一月二十號"
         }
       ]
     }
